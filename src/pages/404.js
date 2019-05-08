ea91255fea3b03c036af1d8233123a774d95bca5
Mark Jenkinson <mark@togglemedia.com>
2019-05-08 14:00:04 -0400

    center 404 page text

--- src/pages/404.js
+++ src/pages/404.js
@@ -10,9 +10,10 @@ class NotFoundPage extends React.Component {
 	render() {
 		return (
 		  <Layout location={this.props.location}>
-			<h1>NOT FOUND</h1>
-			<p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-			<p  className="align-center"><Link to="/">Home</Link></p>
+			<div className="logo faithsourced" alt="Faith Sourced Logo"></div>
+			<h1 className="align-center">NOT FOUND</h1>
+			<p className="align-center">You just hit a route that doesn&#39;t exist... the sadness.</p>
+			<p className="align-center"><Link to="/">Home</Link></p>
 		  </Layout>
 		)
 	}
