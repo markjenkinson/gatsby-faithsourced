dd98f171863501eaf0af1284dad95c34cf9a4e2f
Mark Jenkinson <mark@togglemedia.com>
2019-05-08 13:58:07 -0400

    Add a home link to 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import { Link } from 'gatsby'
+
 import Layout from '../components/layout'
 
 class NotFoundPage extends React.Component {
@@ -10,6 +12,7 @@ class NotFoundPage extends React.Component {
 		  <Layout location={this.props.location}>
 			<h1>NOT FOUND</h1>
 			<p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+			<p  className="align-center"><Link to="/">Home</Link></p>
 		  </Layout>
 		)
 	}
