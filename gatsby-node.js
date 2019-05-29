95f0e899bad6fcc2d5f2f541eaad1b1b9a396cbe
Mark Jenkinson <mark@togglemedia.com>
2019-05-29 00:13:33 -0400

    enable blog

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,4 +4,88 @@
  * See: https://www.gatsbyjs.org/docs/node-apis/
  */
 
-// You can delete this file if you're not using it
+const path = require("path")
+const { createFilePath, createFileNode, createRemoteFileNode} = require(`gatsby-source-filesystem`)
+
+exports.createPages = ({ actions, graphql }) => {
+    const { createPage } = actions
+    return new Promise((resolve, reject) => {
+		resolve(graphql(`{
+				mdBlogs: allMarkdownRemark(
+					sort: { order: DESC, fields: [frontmatter___date] }
+					limit: 1000
+				) {
+					edges {
+						node {
+							fields {
+								slug
+							}
+						}
+					}
+				}
+				jsonBlogs: allThirdPartyPosts(
+					sort: { order: DESC, fields: [date] }
+					limit: 1000
+				) {
+					edges {
+						node {
+							title
+							slug
+						}
+					}		
+				}
+			}
+			`).then(result => {
+				if (result.errors) {
+					console.log(result.errors)
+					return reject(result.errors)
+				}
+
+				const mdBlogTemplate = path.resolve('./src/templates/blog-post.js')
+
+				result.data.mdBlogs.edges.forEach(({ node }) => {
+					createPage({
+						path: node.fields.slug,
+						component: mdBlogTemplate,
+						context: {
+							slug: node.fields.slug,
+						},
+					})
+				})
+				
+				const jsonBlogTemplate = path.resolve('./src/templates/blog-post-json.js')
+				const posts = result.data.jsonBlogs.edges
+				
+				posts.forEach(({ node }, index) => {
+					const prev = index === posts.length - 1 ? null : posts[index + 1].node
+					const next = index === 0 ? null : posts[index - 1].node
+					
+					createPage({
+						path: `${node.slug}`,
+						component: jsonBlogTemplate,
+						context: {
+							slug: node.slug,
+							prev,
+							next,
+						},
+					})
+				})
+				return 
+			})
+		)}
+	)	
+}
+
+
+exports.onCreateNode = ({ node, getNode, actions }) => {
+    const { createNodeField } = actions
+    
+    if (node.internal.type === `MarkdownRemark`) {
+        const slug = createFilePath({ node, getNode, basePath: `pages` })
+        createNodeField({
+            node,
+            name: `slug`,
+            value: `blog${slug}`,
+        })
+	}
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,8 @@
 import React from 'react'
+import { Link, graphql } from 'gatsby'
 import Layout from '../components/layout'
+
+import scrollToComponent from 'react-scroll-to-component';
 
 import Header from '../components/Header'
 import Main from '../components/Main'
@@ -17,10 +20,11 @@ class IndexPage extends React.Component {
       loading: 'is-loading',
       scrolly: 0
     }
-    this.handleOpenArticle = this.handleOpenArticle.bind(this)
-    this.handleCloseArticle = this.handleCloseArticle.bind(this)
+    this.handleOpenArticle = this.handleOpenArticle.bind(this);
+    this.handleCloseArticle = this.handleCloseArticle.bind(this);
     this.setWrapperRef = this.setWrapperRef.bind(this);
     this.handleClickOutside = this.handleClickOutside.bind(this);
+    this.handleScrollTo = this.handleScrollTo.bind(this);
   }
 
   componentDidMount () {
@@ -92,13 +96,19 @@ class IndexPage extends React.Component {
       }
     }
   }
-
+  
+  handleScrollTo() {
+    scrollToComponent(this.Blog, { offset: 0, align: 'top', duration: 300});
+  }
+  
   render() {
+  	//const postList = this.props.data.allMarkdownRemark;
+  	const postListJSON = this.props.data.allThirdPartyPosts;
     return (
       <Layout location={this.props.location}>
         <div className={`body ${this.state.loading} ${this.state.isArticleVisible ? 'blurred' : ''}`}>
           <div id="wrapper">
-            <Header onOpenArticle={this.handleOpenArticle} timeout={this.state.timeout} />
+            <Header onOpenArticle={this.handleOpenArticle} onScrollTo={this.handleScrollTo} timeout={this.state.timeout} />
             <Main
               isArticleVisible={this.state.isArticleVisible}
               timeout={this.state.timeout}
@@ -107,7 +117,36 @@ class IndexPage extends React.Component {
               onCloseArticle={this.handleCloseArticle}
               setWrapperRef={this.setWrapperRef}
             />
-            <Home onOpenArticle={this.handleOpenArticle} timeout={this.state.timeout} />
+            <div id="home" style={this.state.timeout ? {display: 'none'} : {}}>
+            	<Home onOpenArticle={this.handleOpenArticle} />
+            	{/**
+            	<section id="blog">
+            	 {postList.edges.map(({ node }, i) => (
+					<Link to={node.fields.slug} key={i} className="link" >
+					  <div className="post-list">
+						<h1>{node.frontmatter.title}</h1>
+						<span>{node.frontmatter.date}</span>
+						<p>{node.excerpt}</p>
+					  </div>
+					</Link>
+				  ))}
+            	</section>
+            	**/}
+            	<section id="blog" className="tiles" ref={(section) => { this.Blog = section; }}>
+					{postListJSON.edges.map(({ node }, i) => (
+						<article className="image-tile" key={i} style={{backgroundImage: `url(${node.image_1_local.childImageSharp.fluid.src})`}}>
+							<div className="ribbon ribbon-top-right"><div><span><strong>Blog Post</strong></span></div></div>
+							<header className="major">
+								<h3>{node.title}</h3>
+								<span className="date-line"><i className="fa fa-calendar"></i>{node.date}</span>
+								<p>{node.excerpt}</p>
+							 </header>
+							<Link to={`${node.slug}`} key={i} className="link primary"></Link>
+						 </article>
+					))}
+            	</section>
+            	
+            </div>
             <Footer timeout={this.state.timeout} />
           </div>
           <div id="bg"></div>
@@ -118,3 +157,41 @@ class IndexPage extends React.Component {
 }
 
 export default IndexPage
+
+export const listQuery = graphql`
+  query ListQuery {
+    allMarkdownRemark(sort: { order: DESC, fields: [frontmatter___date] }) {
+      edges {
+        node {
+          fields {
+            slug
+          }
+          excerpt(pruneLength: 250)
+          frontmatter {
+            date(formatString: "MMMM D, YYYY [at] h:mm A")
+            title
+          }
+        }
+      }
+    }
+    allThirdPartyPosts(sort: { order: DESC, fields: [date] }) {
+      edges {
+        node {
+			title
+			image_1_url
+			image_1_local {
+				childImageSharp {
+					fluid(maxWidth: 512, maxHeight: 512) {
+						src
+					}
+				}
+				publicURL
+			}
+			date(formatString: "MMMM D, YYYY [at] h:mm A")
+			slug
+			excerpt
+        }
+      }
+    }
+  }
+`
