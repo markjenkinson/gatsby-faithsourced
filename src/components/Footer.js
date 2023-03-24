3c5d677b386789b3619e3e7f7de6bfea340f54f7
Mark Jenkinson <mark@togglemedia.com>
2023-03-24 13:25:31 -0400

    inlined the attribution logo
    to accomodate changes in the color palette, specifically sites that are on dark background versus sites on a light background

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -28,7 +28,7 @@ const Footer = (props) => (
 					<div className='copyright'><p>&copy; 2019. All rights reserved.</p></div>
 				}
 				{process.env.GATSBY_FAITHSITE_ATTRIBUTION === 'true' &&
-					<div className='attribution'><p>Website by <a href="https://faith.dev" target="_blank"> FaithSourced</a>.</p></div>
+					<div className='attribution'><p>Website by <a href="https://faith.dev" target="_blank"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 350 350" class="logo"><path d="M193.2 156.8v-47.3h-36.4v47.3h-36.1v1.7 32.2 2.5h36.1v47.3h36.4v-47.3h36.1v-2.5-32.2-1.7z" class="fill"/><g opacity=".5"><path d="M120.7 142.7L95.1 117l-58 58 58 58 25.6-25.7L88.4 175z" class="fill"/></g><g opacity=".5"><path d="M229.3 142.7l25.6-25.7 58 58-58 58-25.6-25.7 32.3-32.3z" class="fill"/></g></svg> FaithSourced</a>.</p></div>
 				}
 				</>
 			)}
